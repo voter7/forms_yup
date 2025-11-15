80434c324e3e0a2985fc4ab60fb1b2f06ed2fa8f
unknown <email@example.com>
2025-11-16 00:28:45 +0300

    init

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const isDev = mode !== 'production';
 
-  const basePath = isDev ? '/' : '/forms/';
+  const basePath = isDev ? '/' : '/forms_yup/';
 
   return {
     base: basePath,
